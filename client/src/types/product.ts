45c460943f7bed4d34248bd20ba41fe76ae19748
Mursal-Sadigli <sadiqli2024@gmail.com>
2026-07-06 01:28:18 +0400

    Add status, image, category to Product interface

--- client/src/types/product.ts
+++ client/src/types/product.ts
@@ -13,10 +13,13 @@ export interface Product{
     minStock: number;
     unit: 'piece' | 'kg' | 'gram' | 'liter' | 'meter';
     images: string[];
+    image?: string;
     isActive: boolean;
+    status?: string;
     isFeatured: boolean;
     tags: string[];
     supplier?: string;
+    category?: string;
     createdAt: Date;
     updateAt: Date;
 }
